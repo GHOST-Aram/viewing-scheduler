16517cef8d71d285447677c05b6a26cbd519ef4a
GHOST-Aram <owinoojala@gmail.com>
2024-02-24 04:23:39 +0300

    chore: Write tests for PUT routes
          - Expect method not allowed response for batch update  request
          - Expect validation errors if reference Id or input is invalid
          - Expect status 200 and the url of the updated document in the location
            header of the response
          - Expect status 201 and the url of a newly created document in the locaton header
            if the update target does not exist

---
+++ src/test/put.test.ts
@@ -0,0 +1,55 @@
+import { describe, test } from "@jest/globals";
+import request from "supertest"
+import { app } from "./config/test.config";
+import { postData, invalidData } from "./data/data";
+import { assert } from "../z-library/testing/response-assertion";
+
+describe('Viewing scheduler PUT Requests', () => {
+    test('Responds with Method not allowed, status 405: User defined Ids not allowed', 
+        async() =>{
+            const response = await request(app).put('/viewing-scheduler')
+                .send(postData)
+
+            assert.respondsWithMethodNotAllowed(response)
+        }
+    )
+
+    test('Responds with validation errors, status 400: Ivalid assetId', 
+        async() => {
+            const response = await request(app).put('/viewing-scheduler/64c9e4f2df7cc0tgd')
+                .send(postData)
+
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+        }
+    )
+
+    test('Responds with validation errors, status 400: Invalid input data.', 
+        async() =>{
+            const response = await request(app).put('/viewing-scheduler/64c9e4f2df7cc072af2ac9e4')
+                .send(invalidData)
+
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+        }
+    )
+
+    test('Responds with created resource, status 201: New document created.',
+        async() =>{
+            const response = await request(app).put('/viewing-scheduler/64c9e4f2df7cc072af2ac9e2')
+                .send(postData)
+
+            assert.respondsWithCreatedResource(response)
+        }
+    )
+
+    test('Responds with updated resource, status 200: Update success', 
+        async() =>{
+            const response = await request(app).put('/viewing-scheduler/64c9e4f2df7cc072af2ac9e4')
+                .send(postData)
+
+            assert.respondsWithSuccess(response)
+            assert.respondsWithUpdatedResource(response)
+        }
+    )
+})
